83fe4ba2c9136400415703ebca6f5a4bd5599aa3
Stavros Kefaleas <kefaleas.stavros@gmail.com>
2017-10-11 19:29:35 +0300

    Commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FollowService} from './services/follow.service';
+import {AppErrorHandler} from './common/app-error-handler';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FollowersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    FollowService,
+    { provide: ErrorHandler, useClass: AppErrorHandler}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
